0193c1ab337341f3422ee8dcd692642ad9168b87
lordReddx <64440429+lordReddx@users.noreply.github.com>
2021-04-20 21:09:50 +0300

    Initial commit

---
+++ src/repowered/mod/MOD.java
@@ -0,0 +1,14 @@
+package sonnicon.mod;
+
+import mindustry.mod.Mod;
+
+public class MOD extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
